7b4755fa2716265548f1c5b088655fd0f278b9ba
Sam Craig <sammidysam@gmail.com>
2013-01-10 20:28:42 -0500

    Start-up

---
+++ src/essentials/Main.java
@@ -0,0 +1,7 @@
+package essentials;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Today is a wonderful day!  I finally created my... precious... respository.  That's all.  It's just a repository.");
+	}
+}
