09bee2ee32e26f10e038f50efa041857a58eeb69
pufan1998 <slmidkiff@slmidkiff.net>
2013-01-11 20:07:06 -0500

    1st commit
    trying it out

--- src/essentials/Main.java
+++ src/essentials/Main.java
@@ -3,5 +3,6 @@ package essentials;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Today is a wonderful day!  I finally created my... precious... respository.  That's all.  It's just a repository.");
+		System.out.println("Horray Nathan(Me) just committed");
 	}
 }
