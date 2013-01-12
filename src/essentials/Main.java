c55298d2ee3e9fa20d06966b7d5849e28a2cc5a4
Sam Craig <sammidysam@gmail.com>
2013-01-11 22:08:25 -0500

    Set Up LWJGL and Slick2d
    I set up LWJGL and Slick2d and got a window going.  Still need tests
    done to see if it works on Mac, but it works on Windows well.  I set the
    target fps to 60, but some people might want to choose their own, so
    should we use a options.txt (or .ini which I've heard is commonly used
    for options) for target FPS?

--- src/essentials/Main.java
+++ src/essentials/Main.java
@@ -1,8 +1,49 @@
 package essentials;
 
-public class Main {
+import java.io.File;
+
+import org.lwjgl.LWJGLUtil;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.Game;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class Main implements Game {
+	public static boolean fullscreen = false;
+	public boolean requestClose = false;
 	public static void main(String[] args){
-		System.out.println("Today is a wonderful day!  I finally created my... precious... respository.  That's all.  It's just a repository.");
-		System.out.println("Horray Nathan(Me) just committed");
+		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
+		System.setProperty("net.java.games.input.librarypath", System.getProperty("org.lwjgl.librarypath"));
+		try {
+			AppGameContainer agc = new AppGameContainer(new Main());
+			if(fullscreen)
+				agc.setDisplayMode(agc.getScreenWidth(), agc.getScreenHeight(), true);
+			if(!fullscreen)
+				agc.setDisplayMode((int)(agc.getScreenWidth() * 0.8), (int)(agc.getScreenHeight() * 0.8), false);
+			agc.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+	public boolean closeRequested() {
+		return !fullscreen;
+//		for whatever reason true just lets pressing x = close.  Thus, while fullscreen = false it should be true so that you can press X on non-fullscreen
+	}
+	public String getTitle() {
+		return "USStrategy";
+	}
+	public void init(GameContainer gc) throws SlickException {
+		gc.setTargetFrameRate(60);
+	}
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		g.drawString("WOrking", 0, 0);
+	}
+	public void update(GameContainer gc, int j) throws SlickException {
+		Input input = gc.getInput();
+		if(input.isKeyDown(Input.KEY_ESCAPE))
+			gc.exit();
 	}
 }
