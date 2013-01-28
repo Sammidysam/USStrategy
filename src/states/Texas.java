9e559d9bb909a8f980ed57444a3ab75000d175b4
pufan1998 <slmidkiff@slmidkiff.net>
2013-01-28 18:47:44 -0500

    Texas

--- src/states/Texas.java
+++ src/states/Texas.java
@@ -7,7 +7,4 @@ public class Texas extends State {
 	public String getName(){
 		return "Texas";
 	}
-	public Image getFlag() throws SlickException{
-		return new Image("res/flags/" + getName() + ".png");
-	}
 }
